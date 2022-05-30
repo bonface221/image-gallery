03977cbf706022537e06e3b0aa7402a9b8cb33f8
[bonface221] <[mainaboniface221@gmail.com]>
2022-05-30 16:28:11 +0300

    deployment to heroku

--- gallery/settings.py
+++ gallery/settings.py
@@ -18,7 +18,7 @@ MODE='prod'
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY="django-insecure-inj(+^)ry5bheg!ti#gmzeeg94wt1l%+ia0z8^5!-v_ga!idiu"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 DISABLE_COLLECTSTATIC = 1
