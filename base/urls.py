14ad0ad54d2252d41e0e5c4731263fff535a4a72
[bonface221] <[mainaboniface221@gmail.com]>
2022-05-30 16:54:07 +0300

    deployment to heroku

--- base/urls.py
+++ base/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views 
 
 urlpatterns =[
-    path('', views.home, name='home'),
-    path('category/<str:pk>' , views.categories, name='category'),
-    path('location/<str:pk>' , views.locations, name='location'),
-    path('image/<str:pk>' , views.single, name='single'),
+    path('/', views.home, name='home'),
+    path('category/<str:pk>/' , views.categories, name='category'),
+    path('location/<str:pk>/' , views.locations, name='location'),
+    path('image/<str:pk>/' , views.single, name='single'),
 ]
