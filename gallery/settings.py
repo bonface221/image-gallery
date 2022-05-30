bf449ff08d9d7803604c7d87d2bc54c256b49597
[bonface221] <[mainaboniface221@gmail.com]>
2022-05-30 22:14:14 +0300

    deployment to heroku

--- gallery/settings.py
+++ gallery/settings.py
@@ -137,7 +137,7 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     BASE_DIR / 'static'
 ]
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
