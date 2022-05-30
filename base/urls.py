5dbf1d92bff20f7255ef7afdd99aeedfde61c51d
[bonface221] <[mainaboniface221@gmail.com]>
2022-05-30 17:00:59 +0300

    deployment to heroku

--- base/urls.py
+++ base/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns =[
     path('', views.home, name='home'),
-    path('category/<str:pk>/' , views.categories, name='category'),
-    path('location/<str:pk>/' , views.locations, name='location'),
-    path('image/<str:pk>/' , views.single, name='single'),
+    path('category/<str:pk>' , views.categories, name='category'),
+    path('location/<str:pk>' , views.locations, name='location'),
+    path('image/<str:pk>' , views.single, name='single'),
 ]
--- gallery/settings.py
+++ gallery/settings.py
@@ -20,7 +20,7 @@ MODE='prod'
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY="django-insecure-inj(+^)ry5bheg!ti#gmzeeg94wt1l%+ia0z8^5!-v_ga!idiu"
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 DISABLE_COLLECTSTATIC = 1
 
 ALLOWED_HOSTS = ['gallery221.herokuapp.com','127.0.0.1']
