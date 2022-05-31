2c6a1ab02d83647a62c5080f73e2596d82cea0a2
[bonface221] <[mainaboniface221@gmail.com]>
2022-05-31 10:09:42 +0300

    ft:added the admin styling

--- gallery/settings.py
+++ gallery/settings.py
@@ -134,9 +134,9 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS=[
-    BASE_DIR/'static'
-]
+# STATICFILES_DIRS=[
+#     BASE_DIR/'static'
+# ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Extra places for collectstatic to find static files.
